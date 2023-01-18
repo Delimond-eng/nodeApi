a7f11defee52c15245ecfe637b1936c1ce673cf0
Delimond-Dev <delimonddev@localhost.localdomain>
2023-01-18 15:09:18 +0100

    node app config

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
- 
+//.env config
+require("dotenv").config();
+
+//express
+const express = require("express");
+const app = express();
+
+//app route
+app.get("/api", (req, res) => {
+  res.json({
+    success: 1,
+    message: "This api rest message",
+  });
+});
+
+//listener
+app.listen(process.env.APP_PORT, () => {
+  console.log("server up and running on port :", process.env.APP_PORT);
+});
